5df2c85ce6a4c2987915cc1ec3720a303e27da54
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2019-09-01 23:20:46 +0300

    Slightly tune RNG

--- lmalloc.c
+++ lmalloc.c
@@ -8,16 +8,13 @@ void *volatile ptr;
 
 static unsigned int g_seed;
 
-// Used to seed the generator.
 static inline void fast_srand(int seed) {
     g_seed = seed;
 }
 
-// Compute a pseudorandom integer.
-// Output value in range [0, 32767]
-static inline int fast_rand(void) {
+static inline int fast_rand_mod4(void) {
     g_seed = (214013*g_seed+2531011);
-    return (g_seed>>16)&0x7FFF;
+    return (g_seed>>30);
 }
 
 int main(int argc, char **argv)
@@ -25,7 +22,7 @@ int main(int argc, char **argv)
   size_t i;
   for (i = 0; i < 100*1000*1000; i++)
   {
-    size_t sz = 1<<(4 + fast_rand()%4);
+    size_t sz = 1<<(4 + fast_rand_mod4());
     ptr = malloc(sz);
     free(ptr);
     ptr = malloc(sz<<1);
