e291e85d560aefb733f0cab7f66d20f9d01064bf
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2019-09-03 14:05:18 +0300

    Speed up large allocs

--- realapp/jmalloccore.c
+++ realapp/jmalloccore.c
@@ -18,6 +18,8 @@ char *arena;
 
 // 16, 32, 64, 128, 256, 512, 1024, 2048
 struct jmalloc_block *blocks[8] = {};
+// 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576
+struct jmalloc_block *blocks2[9] = {};
 
 static inline size_t topages(size_t limit)
 {
@@ -35,6 +37,9 @@ static inline size_t topages(size_t limit)
 const uint8_t lookup[129] = {
 0,0,1,2,2,3,3,3,3,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
 };
+const uint8_t lookup2[256] = {
+0,1,2,2,3,3,3,3,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+};
 
 __attribute__((noinline)) void *jmalloc(size_t sz)
 {
@@ -44,48 +49,57 @@ __attribute__((noinline)) void *jmalloc(size_t sz)
   uint8_t lookupval;
   if (unlikely(sz > 2048))
   {
-    ret = mmap(NULL, topages(sz), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    if (unlikely(ret == MAP_FAILED))
+    if (unlikely(sz > 1048576))
     {
-      return NULL;
+      ret = mmap(NULL, topages(sz), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+      if (unlikely(ret == MAP_FAILED))
+      {
+        return NULL;
+      }
+      return ret;
     }
-    return ret;
+    lookupval = lookup2[(sz-1)/4096];
+    ls = &blocks2[lookupval];
+    sz = 1<<(12+lookupval);
   }
-  lookupval = lookup[(sz+15)/16];
-  if (lookupval == 0 && sizeof(struct jmalloc_block) > 16)
+  else
   {
-    lookupval = 1;
+    lookupval = lookup[(sz+15)/16];
+    if (lookupval == 0 && sizeof(struct jmalloc_block) > 16)
+    {
+      lookupval = 1;
+    }
+    if (lookupval == 1 && sizeof(struct jmalloc_block) > 32)
+    {
+      lookupval = 2;
+    }
+    if (lookupval == 2 && sizeof(struct jmalloc_block) > 64)
+    {
+      lookupval = 3;
+    }
+    if (lookupval == 3 && sizeof(struct jmalloc_block) > 128)
+    {
+      lookupval = 4;
+    }
+    if (lookupval == 4 && sizeof(struct jmalloc_block) > 256)
+    {
+      lookupval = 5;
+    }
+    if (lookupval == 5 && sizeof(struct jmalloc_block) > 512)
+    {
+      lookupval = 6;
+    }
+    if (lookupval == 6 && sizeof(struct jmalloc_block) > 1024)
+    {
+      lookupval = 7;
+    }
+    if (sizeof(struct jmalloc_block) > 2048)
+    {
+      abort();
+    }
+    ls = &blocks[lookupval];
+    sz = 1<<(4+lookupval);
   }
-  if (lookupval == 1 && sizeof(struct jmalloc_block) > 32)
-  {
-    lookupval = 2;
-  }
-  if (lookupval == 2 && sizeof(struct jmalloc_block) > 64)
-  {
-    lookupval = 3;
-  }
-  if (lookupval == 3 && sizeof(struct jmalloc_block) > 128)
-  {
-    lookupval = 4;
-  }
-  if (lookupval == 4 && sizeof(struct jmalloc_block) > 256)
-  {
-    lookupval = 5;
-  }
-  if (lookupval == 5 && sizeof(struct jmalloc_block) > 512)
-  {
-    lookupval = 6;
-  }
-  if (lookupval == 6 && sizeof(struct jmalloc_block) > 1024)
-  {
-    lookupval = 7;
-  }
-  if (sizeof(struct jmalloc_block) > 2048)
-  {
-    abort();
-  }
-  ls = &blocks[lookupval];
-  sz = 1<<(4+lookupval);
 
   if (unlikely(!*ls))
   {
@@ -119,43 +133,53 @@ __attribute__((noinline)) void jmfree(void *ptr, size_t sz)
   uint8_t lookupval;
   if (unlikely(sz > 2048))
   {
-    munmap(ptr, topages(sz));
-    return;
+    if (unlikely(sz > 1048576))
+    {
+      munmap(ptr, topages(sz));
+      return;
+    }
+    lookupval = lookup2[(sz-1)/4096];
+    ls = &blocks2[lookupval];
+    sz = 1<<(12+lookupval);
   }
-  lookupval = lookup[(sz+15)/16];
-  if (lookupval == 0 && sizeof(struct jmalloc_block) > 16)
+  else
   {
-    lookupval = 1;
+    lookupval = lookup[(sz+15)/16];
+    if (lookupval == 0 && sizeof(struct jmalloc_block) > 16)
+    {
+      lookupval = 1;
+    }
+    if (lookupval == 1 && sizeof(struct jmalloc_block) > 32)
+    {
+      lookupval = 2;
+    }
+    if (lookupval == 2 && sizeof(struct jmalloc_block) > 64)
+    {
+      lookupval = 3;
+    }
+    if (lookupval == 3 && sizeof(struct jmalloc_block) > 128)
+    {
+      lookupval = 4;
+    }
+    if (lookupval == 4 && sizeof(struct jmalloc_block) > 256)
+    {
+      lookupval = 5;
+    }
+    if (lookupval == 5 && sizeof(struct jmalloc_block) > 512)
+    {
+      lookupval = 6;
+    }
+    if (lookupval == 6 && sizeof(struct jmalloc_block) > 1024)
+    {
+      lookupval = 7;
+    }
+    if (sizeof(struct jmalloc_block) > 2048)
+    {
+      abort();
+    }
+    ls = &blocks[lookupval];
+    sz = 1<<(4+lookupval);
   }
-  if (lookupval == 1 && sizeof(struct jmalloc_block) > 32)
-  {
-    lookupval = 2;
-  }
-  if (lookupval == 2 && sizeof(struct jmalloc_block) > 64)
-  {
-    lookupval = 3;
-  }
-  if (lookupval == 3 && sizeof(struct jmalloc_block) > 128)
-  {
-    lookupval = 4;
-  }
-  if (lookupval == 4 && sizeof(struct jmalloc_block) > 256)
-  {
-    lookupval = 5;
-  }
-  if (lookupval == 5 && sizeof(struct jmalloc_block) > 512)
-  {
-    lookupval = 6;
-  }
-  if (lookupval == 6 && sizeof(struct jmalloc_block) > 1024)
-  {
-    lookupval = 7;
-  }
-  if (sizeof(struct jmalloc_block) > 2048)
-  {
-    abort();
-  }
-  ls = &blocks[lookupval];
   blk->u.next = *ls;
   *ls = blk;
 }
