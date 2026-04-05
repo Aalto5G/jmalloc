ade6d13ead0d73199e4d9fb284cbb3108bb81d22
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-04-05 21:18:30 +0300

    Have likely and unlikely always available

--- lib/jmalloclikely.h
+++ lib/jmalloclikely.h
@@ -5,6 +5,9 @@
   #if __has_builtin (__builtin_expect)
     #define jmalloc_likely(x)       __builtin_expect(!!(x),1)
     #define jmalloc_unlikely(x)     __builtin_expect(!!(x),0)
+  #else
+    #define likely(x)       (!!(x))
+    #define unlikely(x)     (!!(x))
   #endif
 #else
   #ifdef __clang__ // LLVM 3.0 is the first to use this, no-op on LLVM 1.0
--- likely.h
+++ likely.h
@@ -5,6 +5,9 @@
   #if __has_builtin (__builtin_expect)
     #define likely(x)       __builtin_expect(!!(x),1)
     #define unlikely(x)     __builtin_expect(!!(x),0)
+  #else
+    #define likely(x)       (!!(x))
+    #define unlikely(x)     (!!(x))
   #endif
 #else
   #ifdef __clang__ // LLVM 3.0 is the first to use this, no-op on LLVM 1.0
